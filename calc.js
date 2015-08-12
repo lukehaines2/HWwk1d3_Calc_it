ca3db128844a6594d070f45068c4a1468eda88cd
LukeHaines <lukehaines2@gmail.com>
2015-08-12 17:05:28 +0100

    Base files blank

---
+++ calc.js
@@ -0,0 +1,18 @@
+var op = prompt('choose an operation');
+console.log('op', op)
+
+var num = prompt('Choose a number');
+console.log('op', op)
+
+var goodNum = parseFloat(num);
+console.log(goodNum)
+
+alert("Why is your console not open?")
+
+
+parseInt(num)   change number to not string
+
+typeof "5" 
+// equals string '5' not 5
+
+parseFloat(num) change other stuff to not string
