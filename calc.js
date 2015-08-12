831e60ffd81f206fd0a4f88131a5bec34853371e
LukeHaines <lukehaines2@gmail.com>
2015-08-12 22:09:42 +0100

    Adjusting page load first

--- calc.js
+++ calc.js
@@ -1,61 +1,64 @@
+// var loop = true;
+// while (loop == true) {
 
-var Advanced_calc = prompt('Do you want (A)dvanced calculator, or (B)asic calculator?');
-console.log('Advanced_calc', Advanced_calc)
+  var Advanced_calc = prompt('Do you want (A)dvanced calculator, or (B)asic calculator?');
+  console.log('Advanced_calc', Advanced_calc)
 
-var num1 = prompt('Choose a number');
-console.log('num1', num1)
+  var num1 = prompt('Choose a number');
+  console.log('num1', num1)
 
-var op = prompt('choose (p)lus + , (m)inus - , (d)ivide / , (t)imes * , (sqrt) , or (p)ower');
-console.log('op', op)
+  var op = prompt('choose (p)lus + , (m)inus - , (d)ivide / , (t)imes * , (sqrt) , or (p)ower');
+  console.log('op', op)
 
-if ( op != 'sqrt') {
-  var num2 = prompt('Choose another number');
-  console.log('num2', num2)
-}
-// if num op isnt sqrt then run 
+  if ( op != 'sqrt') {
+      var num2 = prompt('Choose another number');
+      console.log('num2', num2)
+  }
+  // if num op isnt sqrt then run 
 
-var parseInt1 = parseFloat(num1);
-var parseInt2 = parseFloat(num2);
+  var parseInt1 = parseFloat(num1);
+  var parseInt2 = parseFloat(num2);
 
-// MY CODE
-if (op == '+' || op == 'p') {
-  console.log(parseInt1)
-  console.log(parseInt2)
-  var result = (parseInt1 + parseInt2);
-  alert("Your result is : " + result);
+  // MY CODE
+  if (op == '+' || op == 'p') {
+      console.log(parseInt1)
+      console.log(parseInt2)
+      var result = (parseInt1 + parseInt2);
+      alert("Your result is : " + result);
 
-} else if(op == '-' || op == 'm') {
-  console.log(parseInt1)
-  console.log(parseInt2)
-  var result = (parseInt1 - parseInt2);
-  alert("Your result is : " + result);
+  } else if(op == '-' || op == 'm') {
+      console.log(parseInt1)
+      console.log(parseInt2)
+      var result = (parseInt1 - parseInt2);
+      alert("Your result is : " + result);
 
-} else if(op == "/" || op == 'd') {
-  console.log(parseInt1)
-  console.log(parseInt2)
-  var result = (parseInt1 / parseInt2);
-  alert("Your result is : " + result);
+  } else if(op == "/" || op == 'd') {
+      console.log(parseInt1)
+      console.log(parseInt2)
+      var result = (parseInt1 / parseInt2);
+      alert("Your result is : " + result);
 
-} else if(op == '*' || op == 't') {
-  console.log(parseInt1)
-  console.log(parseInt2)
-  var result = (parseInt1 * parseInt2)
-  alert("Your result is : " + result);
+  } else if(op == '*' || op == 't') {
+      console.log(parseInt1)
+      console.log(parseInt2)
+      var result = (parseInt1 * parseInt2)
+      alert("Your result is : " + result);
 
-} else if(op == "sqrt") {
-  console.log(parseInt1)
-  var result = (Math.sqrt(parseInt1))
-  alert("Your result is : " + result);
+  } else if(op == "sqrt") {
+      console.log(parseInt1)
+      var result = (Math.sqrt(parseInt1));
+      alert("Your result is : " + result);
 
-} else if(op == "p") {
-  console.log(parseInt1)
-  console.log(parseInt2)
-  var result = (MMath.pow(parseInt1, parseInt2))
-  alert("Your result is : " + result);
+  } else if(op == "p") {
+      console.log(parseInt1)
+      console.log(parseInt2)
+      var result = Math.pow(parseInt1, parseInt2);
+      alert("Your result is : " + result);
 
-} else {
-    var op = prompt("Invalid input, please enter correct command");
-}
-
+  } else {
+      var op = prompt("Invalid input, please enter correct command");
+  }
+// break;
+// }
 
   
